d83fe64836fe4be44ca40bf6fed8e76e4c2f1dcb
freelancing with programming <iosattbsoft2021@gmail.com>
2024-11-13 12:59:22 +0500

    nn

--- src/components1/layout/Navbar1.tsx
+++ src/components1/layout/Navbar1.tsx
@@ -1,13 +1,13 @@
 'use client'
-
+import { useState } from "react";
 import useScrollPos from "@/hooks/useScrollPos";
 import MenuItem from "../navbar/elements/navbarItem";
 import ContactBtn from "../navbar/elements/ContactBtn";
-
+import Confetti from 'react-confetti';
 function Navbar() {
 
     const isAtTop = useScrollPos();
-
+    
     return <div className={`mobile:hidden sticky -mt-14 top-0 z-50 ${isAtTop?'opacity-100':'opacity-0 hover:opacity-100 transition-opacity duration-500'} flex justify-end items-center h-14 w-full bg-green`} >
         <div className="flex justify-around text-2xl w-1/2 items-center">
             <MenuItem title={"About"} />
--- src/components1/navbar/elements/ContactBtn.tsx
+++ src/components1/navbar/elements/ContactBtn.tsx
@@ -1,28 +1,25 @@
 'use client'
+import useWindowSize from './WindowSize'; // Adjust path if needed
 
 import { useGlobalState } from "@/contexts/GlobalStateContext";
 import { useEffect } from "react";
-
+import { useState } from "react";
+import Confetti from 'react-confetti';
 const ContactBtn = ({title}:{title:string}) => {
-    
-    const { isModalOpen, toggleModal, exitMenu } = useGlobalState();
-
+    const { width, height } = useWindowSize();
+    const [showConfetti, setShowConfetti] = useState(false);
     const handleClick = () => {
-        exitMenu();
-        toggleModal();
+        setShowConfetti(true);
+        // Hide confetti after 3 seconds
+        setTimeout(() => setShowConfetti(false), 6000);  
     }
-
-    useEffect(() => {
-        if (isModalOpen) {
-            document.body.classList.add("overflow-y-hidden")
-        } else {
-            document.body.classList.remove("overflow-y-hidden")
-        }
-    }, [isModalOpen]);
-    
-    return (<button className='flex items-center cursor-pointer'>
+    return (
+    <div>
+    <button className='flex items-center cursor-pointer'>
         <div aria-label={title} className={"transition ease-in-out duration-300 text-center border-2 border-white rounded-3xl px-3 py-1 hover:bg-white hover:text-black"} onClick={handleClick} >{title}</div>
-        </button>
+    </button>
+    {showConfetti && <Confetti width={width} height={height}/>}
+    </div>
     );
 }
 
