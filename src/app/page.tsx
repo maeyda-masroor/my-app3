fabe5ba9332554044af145f376478a682a7c0fcf
freelancing with programming <iosattbsoft2021@gmail.com>
2024-11-13 11:48:02 +0500

    mm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Hero_Section from "@/components/HeroSection1";
+{/*
 import FeatureSection from "@/components/FeatureSection";
 import SkillSection from "@/components/SkillSection";
 import Resume from "@/components/MyResume";
@@ -6,6 +7,7 @@ import Contact from "@/components/ContactMe";
 import Footer from "@/components/Footer";
 import Project from "@/components/Projects";
 import H1 from "@/components/H1";
+*/}
 import Navbar from "../components1/layout/Navbar1";
 import { GlobalStateProvider } from "@/contexts/GlobalStateContext";
 import BurgerMenu from "../components1/navbar/BurgerNavbar";
